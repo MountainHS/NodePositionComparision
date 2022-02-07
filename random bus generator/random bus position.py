bc171e8da29418b1db697e9a1f65b57944067b07
MountainHS <67748022+MountainHS@users.noreply.github.com>
2022-02-07 12:11:30 +0900

    update .gitignore

--- random bus generator/random bus position.py
+++ random bus generator/random bus position.py
@@ -7,11 +7,11 @@ from tqdm import tqdm
 def get_bus_data(path):  
     # bus 데이터 읽기
     bus_data = pd.read_csv(path)
-    print("get bus data", bus_data)
+    print("get bus data", bus_data, "\n", "*"*10)
     
     # bus 개수 가져오기
     row_count = len(bus_data.index)
-    print("bus data row count", row_count, type(row_count))
+    print("bus data row count", row_count, type(row_count), "\n", "*"*10)
     
     additional_width = 0.1
 
@@ -21,7 +21,7 @@ def get_bus_data(path):
     # bus들 리스트 만들고 섞기
     bus_id = list(range(1,row_count+1))
     random.shuffle(bus_id)
-    print("shuffled bus id list:", bus_id)
+    print("shuffled bus id list:", bus_id, "\n", "*"*10)
     bus_coordinate = []
     
     # 섞은 bus들에 좌표 붙이기
@@ -40,8 +40,8 @@ def get_bus_data(path):
     # json 형태로 출력
     bus_coordinate = sorted(bus_coordinate, key = lambda x:(x['bus id']))
     bus_coordinate = pd.DataFrame(bus_coordinate)
-    print(bus_coordinate)
-    bus_coordinate.to_json("./random bus generator/random bus position.json")
+    print(bus_coordinate, "\n", "*"*10)
+    bus_coordinate.to_json("./random bus generator/result/random bus position.json")
 
 
 
